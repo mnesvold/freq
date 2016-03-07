ad3b13a2cc52407e92f7c980e6d6f546c5f75013
Michael Nesvold <mdnesvold@gmail.com>
2016-03-07 12:08:53 -0500

    +freq database models

--- services/web/freq/models.py
+++ services/web/freq/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    title = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+class ProductArea(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+class FeatureRequest(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    client = models.ForeignKey(Client, models.CASCADE)
+    priority = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    target_date = models.DateField(blank=True, null=True)
+    url = models.URLField(blank=True, verbose_name='URL')
+    product_area = models.ForeignKey(ProductArea, models.PROTECT)
+
+    class Meta:
+        ordering = ('priority', 'client')
+        unique_together = ('client', 'priority')
--- services/web/freq/tests.py
+++ services/web/freq/tests.py
@@ -1,3 +1,18 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from freq.models import Client, ProductArea, FeatureRequest
+
+class ModelTests(TestCase):
+    def setUp(self):
+        self.dummy_client = Client.objects.create(title='Anodyne Industries')
+        self.dummy_area = ProductArea.objects.create(title='Reports')
+
+    def test_min_priority(self):
+        with self.assertRaises(ValidationError):
+            req = FeatureRequest.objects.create(
+                    title='Implement feature',
+                    client=self.dummy_client,
+                    priority=0,
+                    product_area=self.dummy_area)
+            req.full_clean()

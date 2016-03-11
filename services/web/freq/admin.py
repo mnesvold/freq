6f218d07bbb3a887e02ca97a51c7ceff87428ced
Michael Nesvold <mdnesvold@gmail.com>
2016-03-11 13:45:54 -0500

    restrict admin priority input
    Add a layer of validation to the admin forms to stop users from saving feature
    requests with priority values that are either nonpositive or larger than the
    count of relevant feature request records.

--- services/web/freq/admin.py
+++ services/web/freq/admin.py
@@ -1,13 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from django.utils.translation import ugettext as _
 
 from freq.models import *
 
 admin.site.register(Client)
 admin.site.register(ProductArea)
 
+class FeatureRequestAdminForm(ModelForm):
+    def clean_priority(self):
+        priority = self.cleaned_data['priority']
+        if priority < 1:
+            raise ValidationError(
+                    _('Priority values must be positive.'),
+                    code='invalid')
+        client = self.cleaned_data['client']
+        req_count = FeatureRequest.objects.filter(client=client).count()
+        if not self.instance.pk:
+            req_count += 1
+        if priority > req_count:
+            raise ValidationError(
+                    _('This client only has %(count)s feature request(s);'
+                      ' priority values may not exceed that number.'),
+                    code='invalid',
+                    params={'count': req_count})
+        return priority
+
 @admin.register(FeatureRequest)
 class FeatureRequestAdmin(admin.ModelAdmin):
     date_hierarchy = 'target_date'
+    form = FeatureRequestAdminForm
     list_display = ('title', 'client', 'priority', 'target_date',
             'product_area')
     list_filter = ('client', 'product_area')

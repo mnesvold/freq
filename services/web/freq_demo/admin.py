bcb1b8ecfc08214494836d44cb54a8617bba76c7
Michael Nesvold <mdnesvold@gmail.com>
2016-03-11 14:16:56 -0500

    allow non-staff users to log in

--- services/web/freq_demo/admin.py
+++ services/web/freq_demo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.forms import AuthenticationForm
 
 def customize_admin():
     admin.site.site_header = 'Feature Request Tracker'
@@ -6,6 +7,10 @@ def customize_admin():
     admin.site.index_title = 'Track Feature Requests with Freq'
     admin.site.site_url = None
 
+    # allow non-staff users to access admin views
     def is_user_active(request):
       return request.user.is_active
     admin.site.has_permission = is_user_active
+
+    # allow non-staff users to log in
+    admin.site.login_form = AuthenticationForm
